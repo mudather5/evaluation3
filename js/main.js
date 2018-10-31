d5a3483a556a85ba4e76d0f35af0d0451d918ed5
mudather5 <mudathermohamed50@gmail.com>
2018-10-31 10:29:09 +0100

    added code of js for main js

--- js/main.js
+++ js/main.js
@@ -13,3 +13,49 @@ function signIn() {
               document.getElementById("home").style.display = "none";
 
           }
+
+          function favicon() {
+                  var checkBox = document.getElementById("Favicon");
+                  var favicon = document.getElementById("favicon");
+
+                    if (checkBox.checked == true){
+                        favicon.style.display = "block";
+                    }else{
+                      favicon.style.display = "none";
+
+                    }
+
+                }
+
+                function theFunction() {
+                  var checkBox = document.getElementById("theCheck");
+                  var wireframe = document.getElementById("wireframe");
+
+                  if (checkBox.checked == true){
+                      wireframe.style.display = "block";
+                  }else{
+                    wireframe.style.display = "none";
+                  }
+                }
+
+                function mobile() {
+                  var checkBox = document.getElementById("mobile");
+                  var mobile_first = document.getElementById("mobile_first");
+
+                  if (checkBox.checked == true){
+                      mobile_first.style.display = "block";
+                  } else {
+                    mobile_first.style.display = "none";
+                  }
+                }
+
+                function dataBase() {
+                    var checkBox = document.getElementById("data_base");
+                    var data = document.getElementById("data");
+
+                  if (checkBox.checked == true){
+                      data.style.display = "block";
+                  }else{
+                    data.style.display = "none";
+                  }
+            }
